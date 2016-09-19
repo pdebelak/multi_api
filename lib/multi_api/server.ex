071c7c7cb2af74ed2d4cd4808286f68497a1ba6b
pdebelak <pdebelak@gmail.com>
2016-09-19 08:12:12 -0500

    Add helpful howto pages at get /

--- lib/multi_api/server.ex
+++ lib/multi_api/server.ex
@@ -1,6 +1,8 @@
 defmodule MultiApi.Server do
   import Plug.Conn
 
+  @usage ~s<Usage: Post urls as array to `/`. Response will be in form `[{"url":URL,"response":RESPONSE},...etc]`.>
+
   def init(default_opts) do
     IO.puts "starting up MultiApi..."
     default_opts
@@ -8,15 +10,28 @@ defmodule MultiApi.Server do
 
   def call(conn, _opts) do
     conn |>
-    route(conn.method, conn.path_info)
+    route(conn.method, conn.path_info, conn |> get_req_header("content-type"))
   end
 
-  defp route(conn, "POST", []) do
+  defp route(conn, "POST", [], _) do
     conn |>
     respond(200, MultiApi.Fetch.fetch(body(conn)))
   end
 
-  defp route(conn, _, _) do
+  defp route(conn, "GET", [], ["application/json"]) do
+    conn |>
+    respond(200, @usage)
+  end
+
+  require EEx
+  EEx.function_from_file :defp, :template_home_page, "templates/home.eex", [:url]
+  defp route(conn, "GET", [], _) do
+    conn |>
+    put_resp_content_type("text/html") |>
+    send_resp(200, template_home_page(req_url(conn)))
+  end
+
+  defp route(conn, _, _, _) do
     conn |>
     respond(404, %{ error: "Not found" })
   end
@@ -32,5 +47,14 @@ defmodule MultiApi.Server do
       {:ok, response, _} -> MultiApi.TryParse.parse(response)
       _ -> ""
     end
+  end
+
+  defp req_url(%Plug.Conn{scheme: scheme, host: host, port: port}) do
+    port_repr = case {scheme, port} do
+      {:http, 80} -> ""
+      {:https, 443} -> ""
+      {_, port} -> ":#{port}"
+    end
+    "#{scheme}://#{host}#{port_repr}/"
   end
 end
--- test/multi_api/server_test.exs
+++ test/multi_api/server_test.exs
@@ -2,13 +2,23 @@ defmodule ServerTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
-  test "get to root path is 404" do
+  test "get to root path is usage instructions as application/json" do
+    conn = conn(:get, "/")
+    |> put_req_header("content-type", "application/json")
+    |> MultiApi.Server.call([])
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert Regex.match?(~r/usage/i, Poison.decode!(conn.resp_body))
+  end
+
+  test "get to root path is home page for other content types" do
     conn = conn(:get, "/")
     |> MultiApi.Server.call([])
 
     assert conn.state == :sent
-    assert conn.status == 404
-    assert Poison.decode!(conn.resp_body) == %{"error" => "Not found"}
+    assert conn.status == 200
+    assert Regex.match?(~r/welcome/i, conn.resp_body)
   end
 
   test "get to other path is 404" do
