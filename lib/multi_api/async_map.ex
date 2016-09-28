d86008905be06127c59d4343691461ec39c7e156
pdebelak <pdebelak@gmail.com>
2016-09-27 20:29:02 -0500

    Update async map to use Task.start_link

--- lib/multi_api/async_map.ex
+++ lib/multi_api/async_map.ex
@@ -1,14 +1,14 @@
 defmodule MultiApi.AsyncMap do
   def async_map(list, async) do
     list |>
-    Enum.map(fn (item) -> async_single(item, async) end) |>
+    Enum.map(&async_single(&1, async)) |>
     length() |>
     stream_responses()
   end
 
   defp async_single(item, async) do
     pid = self()
-    spawn_link(fn -> send(pid, async.(item)) end)
+    Task.start_link(fn -> send(pid, async.(item)) end)
     item
   end
 
